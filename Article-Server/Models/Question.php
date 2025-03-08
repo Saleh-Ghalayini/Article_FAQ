402e17d04a3503cf6889438a205776076f0fc3e8
saleh <ghalayinisaleh9@gmail.com>
2025-03-08 20:01:24 +0200

    finished the implementatio of the question model

--- Article-Server/Models/Question.php
+++ Article-Server/Models/Question.php
@@ -5,26 +5,41 @@
 
     class Question{
 
-        public static function createQuestion($QuestionSkeketon, $conn) {
-
+        public static function createQuestion($QuestionSkeleton, $conn) {
             $query = $conn->prepare("INSERT INTO questions (question, answer) VALUES(?, ?);");
-            $query->bind_param("ss", $QuestionSkeketon->get_question(),
-                                      $QuestionSkeketon->get_answer());
+            $query->bind_param("ss", $QuestionSkeleton->get_question(),
+                                      $QuestionSkeleton->get_answer());
             $query->execute();
             $query->close();
         }
+        /*
+        public static function updateQuestion($QuestionSkeleton, $conn) {
+            $query = $conn->prepare("UPDATE questions SET question = ?, answer = ? WHERE question = ? AND answer = ?");
+            $query->bind_param("ss", $QuestionSkeleton->get_question(),
+                                      $QuestionSkeleton->get_answer());
+            $query->execute();
+            $query->close();
+        }*/
 
-        public static function updateQuestion() {
+        public static function readQuestion($QuestionSkeleton, $conn) {
+            $query = $conn->prepare("SELECT question, answer  FROM questions WHERE question = ? AND answer = ?");
+            $query->bind_param("ss", $QuestionSkeleton->get_question(),
+                                      $QuestionSkeleton->get_answer());
+            $query->execute();
+            $result = $query->get_result();
+            if($result->num_rows > 0)
+                return $result->fetch_assoc();
 
+            $query->close();
+            return null;
         }
 
-        public static function readQuestion() {
-
-        }
-
-        public static function deleteQuestion() {
-
+        public static function deleteQuestion($QuestionSkeleton, $conn) {
+            $query = $conn->prepare("DELETE FROM questions WHERE question = ? AND answer = ?");
+            $query->bind_param("ss", $QuestionSkeleton->get_question(),
+                                      $QuestionSkeleton->get_answer());
+            $query->execute();
+            $query->close();
         }
     }
-
 ?>
