6cf7dde545b7c0e8eb365be27a918d87cac5d42f
saleh <ghalayinisaleh9@gmail.com>
2025-03-08 19:43:39 +0200

    initiated the CRUD empty functions

---
+++ Article-Server/Models/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+    require("../Connection/connection.php");
+    require("QuestionSkeleton.php");
+
+    class Question{
+
+        public static function createQuestion($QuestionSkeketon, $conn) {
+
+        }
+
+        public static function updateQuestion() {
+
+        }
+
+        public static function readQuestion() {
+
+        }
+
+        public static function deleteQuestion() {
+
+        }
+    }
+
+?>
