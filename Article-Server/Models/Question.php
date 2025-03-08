69db9f2f00a90d20f2f0879299a2be74cfc2db81
saleh <ghalayinisaleh9@gmail.com>
2025-03-08 19:47:46 +0200

    finished the createQuestion method

--- Article-Server/Models/Question.php
+++ Article-Server/Models/Question.php
@@ -7,6 +7,11 @@
 
         public static function createQuestion($QuestionSkeketon, $conn) {
 
+            $query = $conn->prepare("INSERT INTO questions (question, answer) VALUES(?, ?);");
+            $query->bind_param("ss", $QuestionSkeketon->get_question(),
+                                      $QuestionSkeketon->get_answer());
+            $query->execute();
+            $query->close();
         }
 
         public static function updateQuestion() {
