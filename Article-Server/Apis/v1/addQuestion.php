969b85123c711aba524ca4196767c130e37b8b64
saleh <ghalayinisaleh9@gmail.com>
2025-03-09 12:16:14 +0200

    finished the addQuestion api build

---
+++ Article-Server/Apis/v1/addQuestion.php
@@ -0,0 +1,28 @@
+<?php
+
+    require("../../Connection/connection.php");
+    require_once("../../Models/QuestionSkeleton.php");
+    require("../../utils/utilities.php");
+    require("../../Models/Question.php");
+
+    if(!isset($_GET["question"]) || !isset($_GET["answer"])) {
+        http_response_code(400);
+
+        Utilities::response("Failed", "Question and answer are required");
+    }
+
+    $question = $_GET["question"];
+    $answer = $_GET["answer"];
+
+    try {
+        $question_obj = new QuestionSkeleton($question, $answer);
+        Question::createQuestion($question_obj, $conn);
+
+        Utilities::response("Succeed", "Question got inserted into the Database");
+    } catch (Throwable $e) {
+        http_response_code(400);
+
+        Utilities::response("Failed", $e);
+    }
+
+?>
--- Article-Server/Apis/v1/login.php
+++ Article-Server/Apis/v1/login.php
@@ -7,9 +7,8 @@
     $body = json_decode(file_get_contents("php://input"), true);
 
     if(!isset($body["email"]) || !isset($body["password"])) {
-
         http_response_code(400);
-
+        
         Utilities::response("Failed", "email and password are required");
     }
 
@@ -24,9 +23,10 @@
         $result = $query->get_result();
         $user = $result->fetch_assoc();
         $hashed_password = hash('sha256', $password);
-        if($hashed_password === $user["password"]) {
+
+        if($hashed_password === $user["password"])
             Utilities::response("Succeed", $user);
-        } else {
+        else {
             http_response_code(401);
 
             Utilities::response("Failed", "invalid credentials");
